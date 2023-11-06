91975e8247977859715da313e8e40e23b29e3aa7
nineslop <vladislav.ivanov.2004@bk.ru>
2023-11-06 12:26:58 +0300

    Eiler

---
+++ function Eiler/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        final int n = 3240;
+        int result = EulerTotientFunction.phi(n);
+        System.out.println("phi(" + n + ") = " + result);
+    }
+}
